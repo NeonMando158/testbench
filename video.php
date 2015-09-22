44efbc4d04794231a74548a579c93cbc07e2d524
Vrandesh Bandikatti <vrandesh.bandikatti@apptarix.com>
2015-09-22 11:15:31 +0530

    changes in code 21st sep 2015

--- video.php
+++ video.php
@@ -207,11 +207,11 @@
     			</li>
           <li class="dropdown mega-dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Videos</a>        
-            <ul class="dropdown-menu mega-dropdown-menu">
+            <ul class="dropdown-menu mega-dropdown-menu custompageclicked" style="display: visible;">
               <li><a href="#">Auto Carousel</a></li>
                   <li><a href="#">Carousel Control</a></li>
                   <li><a href="#">Left & Right Navigation</a></li>
-                  <li><a href="video.php">Capitulo 99</a></li>
+                  <li><a href="video.php" style="color: red; cursor: pointer;">Capitulo 99</a></li>
                   <li><a href="#">Glyphicon</a></li>
                   <li><a href="#">Google Fonts</a></li>
               
@@ -242,10 +242,49 @@
     </div>
   </div>
   <div class="container" style="margin-top: 30px; ">
-    <div class="col-md-12" style="margin-bottom: 10px;">
+    <!-- <div class="col-md-12" style="margin-bottom: 10px;">
         <img src="images/banner.png" alt="banner" style="width: 100%;"/>
+    </div> -->
+    <!-- main program video wrapper -->
+    <div class="col-md-12" style="border: 1px solid lightgrey; padding: 0px;">
+        <div class="col-md-8" style="padding: 0px;">
+            <div class="col-md-12" style="padding: 0px;">
+              <!-- <img src="images/video.png" alt="video" style="width: 100%;"/> -->
+<!-- 
+              <video width="400" controls>
+                <source src="mov_bbb.mp4" type="video/mp4">
+                <source src="mov_bbb.ogg" type="video/ogg">
+                Your browser does not support HTML5 video.
+              </video> -->
+              <iframe width="813" height="415" src="https://www.youtube.com/embed/G4eoUTkD0MQ" frameborder="0" allowfullscreen></iframe>
+            </div>
+
+            <div class="col-md-12" style="padding: 0px;">
+              <div class="col-md-2 customwell">PARTE: </div>
+              <div class="col-md-2 customwell">1</div>
+              <div class="col-md-2 customwell">2</div>
+              <div class="col-md-2 customwell">3</div>
+              <div class="col-md-2 customwell">4</div>
+              <div class="col-md-2 customwell">5</div>
+            </div>
+        </div>
+        <div class="col-md-4" style="padding: 0px;">
+          <div class="col-md-6" style="background: red none repeat scroll 0% 0%; color: white; font-weight: bold; text-align: left; padding: 3px 8px; font-size: 18px;">ULTIMO CAPITULO</div>
+          <div class="col-md-12" style="border-bottom: 1px solid lightgrey">
+              <span style="margin-top: 20px; font-weight: bold; font-size: 14px; color: red; text-transform: lowercase;">EL SenoR DE LOS CApitulos</span><br/>
+              <span style="font-size: 30px; font-weight: bold; color: black;">Capitulo 99</span><br/>
+              <span style="font-size: 12px; color: grey;">EMITIDO : LUNES, 09/14/2015</span><br/>
+              <span style="font-size: 14px; color: #222; text-align: justify">Casillas le dice a Venegas que le va cobrar meterse con su familia</span><br/>
+          </div>
+        </div>
     </div>
-    <div class="col-md-4 column1">
+    <div class="col-md-12 ">
+
+    </div>
+    <div class="col-md-12 ">
+
+    </div>
+    <!-- <div class="col-md-4 column1">
         <div class="well programs">
           <span class="program-type"><a href="#">DECISION 2016</a></span>
           <h5 class="program-title">Hillary Clinton popular entre los latinos</h5>
@@ -255,18 +294,18 @@
         <div class="well programs"><img src="http://placehold.it/354x200/2ecc71/f5f5f5/&text=Program+3" class="img-responsive" alt="program 3"></div>
         <div class="well programs"><img src="http://placehold.it/354x200/2ecc71/f5f5f5/&text=Program+4" class="img-responsive" alt="program 4"></div>
         <div class="well programs"><img src="http://placehold.it/354x200/2ecc71/f5f5f5/&text=Program+5" class="img-responsive" alt="program 5"></div>
-    </div>
-    <div class="col-md-4 column2">
+    </div> -->
+    <!-- <div class="col-md-4 column2">
       <div class="well"><img src="images/ad1.png" style="width: 100%;" class="img-responsive" alt="adv 1"></div>
       <div class="well programs"><img src="http://placehold.it/354x200/d9534f/f5f5f5/&text=Program+6" class="img-responsive" alt="program 6"></div>
       <div class="well programs"><img src="http://placehold.it/354x100/1caf9a/f5f5f5/&text=Adv+2" class="img-responsive" alt="adv 2"></div>
       <div class="well programs"><img src="http://placehold.it/354x200/f0ad4e/f5f5f5/&text=Program+6" class="img-responsive" alt="program 6"></div>
-    </div>
-    <div class="col-md-4 column3">
+    </div> -->
+    <!-- <div class="col-md-4 column3">
       <div class="well programs"><img src="http://placehold.it/354x354/f0ad4e/f5f5f5/&text=Adv+1" class="img-responsive" alt="adv 1"></div>
       <div class="well programs"><img src="http://placehold.it/354x354/d9534f/f5f5f5/&text=Program+7" class="img-responsive" alt="program 6"></div>
       <div class="well programs"><img src="http://placehold.it/354x100/f0ad4e/f5f5f5/&text=Adv+2" class="img-responsive" alt="adv 2"></div>
-    </div>
+    </div> -->
   </div>
   <script type="text/javascript" src="http://connect.facebook.net/en_US/sdk.js"></script>
   <script src="http://mydrive.apptarix.com:8080/apptajs/appta-min.js"></script>
@@ -277,6 +316,11 @@
       var apptaAgent = new ApptaAgent();
       apptaAgent.login(api_key); 
     }
+    $(document).ready( function(){
+      $(".custompageclicked").show(); 
+    });
+      
+    
   </script>
 </body>
 </html>
