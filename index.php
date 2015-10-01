14de8762ca80624cec8de516dd9b26101b68042b
Vrandesh Bandikatti <vrandesh.bandikatti@apptarix.com>
2015-10-01 15:41:58 +0530

    menu restructuring

--- index.php
+++ index.php
@@ -28,150 +28,14 @@
     	
     	<div class="collapse navbar-collapse js-navbar-collapse">
     		<ul class="nav navbar-nav">
-    			<li class="dropdown mega-dropdown">
-    				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Novels</a>				
-    				<ul class="dropdown-menu mega-dropdown-menu">
-    					<li class="col-sm-3">
-    						<ul>
-    							<li class="dropdown-header"></li>                            
-                    <div id="menCollection" class="carousel slide" data-ride="carousel">
-                      <div class="carousel-inner">
-                        <div class="item active">
-                            <a href="#"><img src="http://placehold.it/254x150/ff3546/f5f5f5/&text=New+Collection" class="img-responsive" alt="programs 1"></a>
-                            <h4><small>Some Text</small></h4>                                        
-                            <!-- <button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> </button>        -->
-                        </div><!-- End Item -->
-                        <div class="item">
-                            <a href="#"><img src="http://placehold.it/254x150/3498db/f5f5f5/&text=New+Collection" class="img-responsive" alt="programs 2"></a>
-                            <h4><small>asdasd</small></h4>                                        
-                            
-                        </div><!-- End Item -->
-                        <div class="item">
-                            <a href="#"><img src="http://placehold.it/254x150/2ecc71/f5f5f5/&text=New+Collection" class="img-responsive" alt="programs 3"></a>
-                            <h4><small>asdasdasded</small></h4>                                        
-                            
-                        </div><!-- End Item -->                                
-                      </div><!-- End Carousel Inner -->
-                      <!-- Controls -->
-                      <a class="left carousel-control" href="#menCollection" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="right carousel-control" href="#menCollection" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </div><!-- /.carousel -->
-                    <li class="divider"></li>
-                    <li><a href="#">View all Programs <span class="glyphicon glyphicon-chevron-right pull-right"></span></a></li>
-    						</ul>
-    					</li>
-    					<li class="col-sm-3">
-    						<ul>
-    							<li class="dropdown-header">INICIO</li>
-    							<li><a href="#">EL sENOR DE lOS cIELOS3 </a></li>
-                  <li><a href="#">Bajo el Mismo Cielo</a></li>
-                  <li><a href="#">Avendia Brasil</a></li>
-                  <li><a href="#">Senora Acero 2</a></li>
-    							<li class="divider"></li>
-    							<li class="dropdown-header">Capitulos</li>
-                  <li><a href="#"></a></li>
-    							<li><a href="#">Google Fonts</a></li>
-    						</ul>
-    					</li>
-    					<li class="col-sm-3">
-    						<ul>
-    							<li class="dropdown-header">Plus</li>
-    							<li><a href="#">Navbar Inverse</a></li>
-    							<li><a href="#">Pull Right Elements</a></li>
-    							<li><a href="#">Coloured Headers</a></li>                            
-    							<li><a href="#">Primary Buttons & Default</a></li>							
-    						</ul>
-    					</li>
-    					<li class="col-sm-3">
-    						<ul>
-    							<li class="dropdown-header">Much more</li>
-                                <li><a href="#">Easy to Customize</a></li>
-    							<li><a href="#">Calls to action</a></li>
-    							<li><a href="#">Custom Fonts</a></li>
-    							<li><a href="#">Slide down on Hover</a></li>                         
-    						</ul>
-    					</li>
-    				</ul>				
-    			</li>
           <li class="dropdown mega-dropdown">
-        	  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shows</a>				
-    				<ul class="dropdown-menu mega-dropdown-menu">
-    					<li class="col-sm-3">
-        					<ul>
-    							<li class="dropdown-header">Features</li>
-    							<li><a href="#">Auto Carousel</a></li>
-                                <li><a href="#">Carousel Control</a></li>
-                                <li><a href="#">Left & Right Navigation</a></li>
-    							<li><a href="#">Four Columns Grid</a></li>
-    							<li class="divider"></li>
-    							<li class="dropdown-header">Fonts</li>
-                                <li><a href="#">Glyphicon</a></li>
-    							<li><a href="#">Google Fonts</a></li>
-    						</ul>
-    					</li>
-    					<li class="col-sm-3">
-    						<ul>
-    							<li class="dropdown-header">Plus</li>
-    							<li><a href="#">Navbar Inverse</a></li>
-    							<li><a href="#">Pull Right Elements</a></li>
-    							<li><a href="#">Coloured Headers</a></li>                            
-    							<li><a href="#">Primary Buttons & Default</a></li>							
-    						</ul>
-    					</li>
-    					<li class="col-sm-3">
-    						<ul>
-    							<li class="dropdown-header">Much more</li>
-                                <li><a href="#">Easy to Customize</a></li>
-    							<li><a href="#">Calls to action</a></li>
-    							<li><a href="#">Custom Fonts</a></li>
-    							<li><a href="#">Slide down on Hover</a></li>                         
-    						</ul>
-    					</li>
-              <li class="col-sm-3">
-        					<ul>
-    							<li class="dropdown-header">Live Program</li>                            
-                    <div id="womenCollection" class="carousel slide" data-ride="carousel">
-                      <div class="carousel-inner">
-                        <div class="item active">
-                            <a href="#"><img src="http://placehold.it/254x150/3498db/f5f5f5/&text=New+Collection" class="img-responsive" alt="program 1"></a>
-                            <h4><small>asdasd</small></h4>                                        
-                            <!-- <button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>        -->
-                        </div><!-- End Item -->
-                        <div class="item">
-                            <a href="#"><img src="http://placehold.it/254x150/ff3546/f5f5f5/&text=New+Collection" class="img-responsive" alt="program 2"></a>
-                            <h4><small>asdasdasd</small></h4>                                        
-                            <!-- <button class="btn btn-primary" type="button">9,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>         -->
-                        </div><!-- End Item -->
-                        <div class="item">
-                            <a href="#"><img src="http://placehold.it/254x150/2ecc71/f5f5f5/&text=New+Collection" class="img-responsive" alt="program 3"></a>
-                            <h4><small>asdasded</small></h4>                                        
-                            <!-- <button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>       -->
-                        </div><!-- End Item -->                                
-                      </div><!-- End Carousel Inner -->
-                      <!-- Controls -->
-                      <a class="left carousel-control" href="#womenCollection" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="right carousel-control" href="#womenCollection" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </div><!-- /.carousel -->
-                    <li class="divider"></li>
-                    <li><a href="#">View all Collection <span class="glyphicon glyphicon-chevron-right pull-right"></span></a></li>
-    						</ul>
-    					</li>
-    				</ul>				
-    			</li>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Novelas</a>       
+          </li>
           <li class="dropdown mega-dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Videos</a>        
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shows</a>        
+          </li>
+          <li class="dropdown mega-dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Videos</a>        
             <ul class="dropdown-menu mega-dropdown-menu">
               <li><a href="#">Auto Carousel</a></li>
                   <li><a href="#">Carousel Control</a></li>
@@ -196,7 +60,7 @@
             </a>
           </li>
           <li style="background: #3b5958; padding: 11px; cursor: pointer;">
-            <a onclick="fbLogin()" style="padding: 0px;">
+            <a onclick="facebookLogin()" style="padding: 0px;">
               <i class="fa fa-facebook" style="background: white none repeat scroll 0% 0%; color: rgb(59, 89, 88); padding: 4px 9px;font-size: 21px; border-radius: 50px;"></i>
               <span class="customfblogin" style="font-size: 15px; text-transform: lowercase;">login</span>
             </a>
@@ -204,7 +68,7 @@
           <li class="userimage" style="background: #9cbdff; padding: 3px;">
             <a style="padding: 3px;" class="userimagecontainer" href="#"><i class="fa fa-user" style="font-size: 36px; padding: 2px;"></i></a>
           </li>
-    		</ul>
+        </ul>
     	</div><!-- /.nav-collapse -->
       </nav>
     </div>
