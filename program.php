1c253e9a8685040cbfa67277c840f32281c2ae60
Apptarix <Apptarix@apptarix.com>
2015-10-30 13:25:34 +0530

    changes and bug fixes

--- program.php
+++ program.php
@@ -323,5 +323,24 @@
 	<!-- twiiter reply modal window closes -->
 
   </div>
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+            <a class="twitter-timeline"  href="https://twitter.com/Telemundo" data-widget-id="659985847412879360">Tweets by @Telemundo</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+          
 </body>
 </html>
